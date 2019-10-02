33dc828479c77fa45360a2fff9b7db694d0eb315
radu200 <raduprodan200@gmail.com>
2019-10-02 21:45:51 +0100

    Updates

---
+++ precache-manifest.775f31468b3fd9edfd4ec0a3f4d41a02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7d77c72f8eb9156015d6",
+    "url": "/static/js/main.7d77c72f.chunk.js"
+  },
+  {
+    "revision": "b31293bb9904af573e20",
+    "url": "/static/js/2.b31293bb.chunk.js"
+  },
+  {
+    "revision": "7d77c72f8eb9156015d6",
+    "url": "/static/css/main.dec6dc99.chunk.css"
+  },
+  {
+    "revision": "92c0c5948a132742cb4b73f789ded0b3",
+    "url": "/index.html"
+  }
+];
